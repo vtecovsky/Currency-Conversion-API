fbc8c8bd1e49e7a6a5b5533e4b789a45dad3d0ea
Saveliy <s.lekhtin@innopolis.university>
2023-10-25 16:29:04 +0300

    fix: remove id field from currency table

--- src/storage/sql/models/currency.py
+++ src/storage/sql/models/currency.py
@@ -5,7 +5,6 @@ from src.storage.sql.models import Base
 
 class Currency(Base):
     __tablename__ = "currency"
-    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
-    code: Mapped[str] = mapped_column()
+    code: Mapped[str] = mapped_column(unique=True, primary_key=True)
     rate: Mapped[float] = mapped_column()
